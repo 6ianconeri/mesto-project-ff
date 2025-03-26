2dbd2e87e8eb49f93bfbf3307e7e75127756d95a
6ianconeri <skodaazz666@gmail.com>
2025-03-26 13:25:15 +0300

    Job is done

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function addCard(array) {
+    initialCards.forEach(function(elem) {
+        const cardTemplate = document.querySelector('#card-template').content;
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        cardElement.querySelector('.card__title').textContent = elem.name;
+        cardElement.querySelector('.card__image').src = elem.link;
+        cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+        placesList.append(cardElement);
+    })
+}
+
+addCard(initialCards);
+
+function deleteCard() {
+    const cardItem = content.querySelector('.places__item');
+    cardItem.remove();
+}
